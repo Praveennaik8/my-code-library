44c734c6b305bbfc10f59129f8462e7df86546c7
Praveen Naik <47112955+Praveennaik8@users.noreply.github.com>
2021-06-22 19:23:05 +0530

    Update Xor_brute.cpp

--- Codes/Xor_brute.cpp
+++ Codes/Xor_brute.cpp
@@ -1,5 +1,3 @@
-// C++ program to print all combination
-// of size r in an array of size n
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -35,18 +33,10 @@ void combinationUtil(int arr[], int data[],
 					int start, int end,
 					int index, int r);
 
-// The main function that prints
-// all combinations of size r
-// in arr[] of size n. This function
-// mainly uses combinationUtil()
 void printCombination(int arr[], int n, int r)
 {
-	// A temporary array to store
-	// all combination one by one
 	int data[r];
 
-	// Print all combination using
-	// temporary array 'data[]'
 	combinationUtil(arr, data, 0, n-1, 0, r);
 }
 
@@ -61,8 +51,6 @@ void combinationUtil(int arr[], int data[],
 					int start, int end,
 					int index, int r)
 {
-	// Current combination is ready
-	// to be printed, print it
 	if (index == r)
 	{
         int temp = data[0];
@@ -77,11 +65,6 @@ void combinationUtil(int arr[], int data[],
 		return;
 	}
 
-	// replace index with all possible
-	// elements. The condition "end-i+1 >= r-index"
-	// makes sure that including one element
-	// at index will make a combination with
-	// remaining elements at remaining positions
 	for (int i = start; i <= end &&
 		end - i + 1 >= r - index; i++)
 	{
@@ -91,7 +74,6 @@ void combinationUtil(int arr[], int data[],
 	}
 }
 
-// Driver code
 signed main()
 {
     fastio
@@ -114,4 +96,3 @@ signed main()
 	
 }
 
-// This code is contributed by rathbhupendra
